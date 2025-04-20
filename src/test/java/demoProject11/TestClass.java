79421157067b437bdf52c279a9acca6d7eb4e690
Ananthi <ananthivenkadesh23@gmail.com>
2025-04-20 23:56:16 +0530

    added remote url

--- src/test/java/demoProject11/TestClass.java
+++ src/test/java/demoProject11/TestClass.java
@@ -1,9 +1,15 @@
 package demoProject11;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,9 +18,16 @@ public class TestClass {
 	public static WebDriver driver;
 	
 	@BeforeMethod
-	public void launchDriver() {
+	public void launchDriver() throws MalformedURLException {
+		/*
+		 * DesiredCapabilities dr=null; dr=DesiredCapabilities.chrome();
+		 * dr.setBrowserName("chrome"); dr.setPlatform(Platform.WIN11);
+		 */
+		ChromeOptions opt=new ChromeOptions();
+		opt.setPlatformName("Windows 11");
+		
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\Driver\\chromedriver.exe");
-		driver=new ChromeDriver();
+		driver=new RemoteWebDriver(new URL(" http://192.168.31.73:4444"),opt);
 		driver.manage().window().fullscreen();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
